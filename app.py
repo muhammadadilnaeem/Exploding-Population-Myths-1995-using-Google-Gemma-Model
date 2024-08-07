de077b015a9d13ca51a35e6e8c03438030bdb57d
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-08-07 09:05:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def vector_embedding():
         st.session_state.docs = st.session_state.loader.load()  # Document Loading
         st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)  # Chunk Creation
         st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:20])  # Splitting
-        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)  # Vector OpenAI embeddings
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)  # Vector embeddings
 
 # Embedding creation button
 with st.container():
